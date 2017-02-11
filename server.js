9289389914d88865b5a2157e816b250a46b99ce8
mattlaguardia <matthewlaguardia@gmail.com>
2017-02-11 13:42:53 -0800

    adds some backend files and logic

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ var express = require('express')
 var path = require('path')
 
 var app = express()
+// Need to add database
+
+
+// ENV file setup
+require('dotenv').config();
 
 // serve our static stuff like index.css
 app.use(express.static(__dirname))
@@ -11,6 +16,17 @@ app.use(express.static(__dirname))
 app.get('*', function (req, res) {
   res.sendFile(__dirname + '/public/index.html')
 })
+
+const googleVisionApi = require('./backend/google-vision.js');
+app.use('/api/vision-res', googleVisionApi);
+
+// Headers config
+const allowCrossDomain = function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+    res.setHeader('Access-Control-Allow-Credentials', true)
+}
 
 app.set('port', (process.env.PORT || 6969));
 app.listen(app.get('port'), function () {
