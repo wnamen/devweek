a4d31334c47b01a64612cf6f15c80fa825d15d4b
mrogach2350 <m.rogach@gmail.com>
2017-02-11 16:09:07 -0800

    updates flowroutes.js with post request

--- backend/flowroutes.js
+++ backend/flowroutes.js
@@ -1,17 +1,24 @@
 var express = require('express');
 var app = express();
 
+var flowRouteKey = process.env.FLOWROUTEAPIKEY;
+var flowRouteSecret = process.env.FLOWROUTEAPISECRET;
+var fromNumber = process.env.FLOWROUTENUMBER;
+
 // Post request to send message
 app.post('https://api.flowroute.com/v2/messages', function (req, res, err) {
   if (err) {
     console.log(err);
   }
   res.send({
-    "to":"7632760478",
-    "from":"18553569768",
+    "to":"17632760478",
+    "from":fromNumber,
     "body":"sample request"
   });
-})
+});
 
+app.get('/responses', function (req,res){
+  res.send('Hello World!');
+});
 
 module.exports = app;
