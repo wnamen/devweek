2ecd6f1c36eb04a9438b859075f6ad7986aac366
mrogach2350 <m.rogach@gmail.com>
2017-02-11 22:30:16 -0800

    fixes payment api

--- backend/flowroutes.js
+++ backend/flowroutes.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
-var db = require('mongodb');
+var Payment = require('./models/payments_model.js');
 
 
 var flowRouteKey = process.env.FLOWROUTEAPIKEY;
@@ -9,11 +9,12 @@ var flowRouteSecret = process.env.FLOWROUTEAPISECRET;
 var fromNumber = process.env.FLOWROUTENUMBER;
 var mlabUri = process.env.MLABADDRESS;
 
+var db = mongoose.connection;
+
+
 // Post request to send message
 app.post('https://api.flowroute.com/v2/messages', function (req, res, err) {
-  if (err) {
-    console.log(err);
-  }
+  if (err) return console.log(err);
   res.send({
     "to":"17632760478",
     "from":fromNumber,
@@ -21,8 +22,19 @@ app.post('https://api.flowroute.com/v2/messages', function (req, res, err) {
   });
 });
 
-app.get('/responses', function (req,res){
-  console.log(db);
+app.post('/payments', function (req,res){
+  console.log('Open!');
+
+  var newPayment = new Payment({
+    amount: req.query.amount,
+    dueDay: req.query.dueDay || 15,
+    reccurring: req.query.reccuring || false,
+    userId: req.query.userId
+  });
+
+  db.collections.payments.save(newPayment);
+  res.send();
+
 });
 
 module.exports = app;
